bef09a3dd49a24c5a7c3c63a00f392a0e7a0e7ed
suborna33 <152749142+suborna33@users.noreply.github.com>
2026-05-23 17:47:57 +0600

    fix commonjs vercel

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,20 +1,20 @@
-import { defineConfig } from "tsup";
+// import { defineConfig } from "tsup";
 
-export default defineConfig({
-  entry: ["src/index.ts"],
-  format: ["esm", "cjs"], // Keep this as ESM
-  target: "esnext",
-  outDir: "dist",
-  clean: true,
-  bundle: true,
-  splitting: false,
-  sourcemap: true,
+// export default defineConfig({
+//   entry: ["src/index.ts"],
+//   format: ["esm"], // Keep this as ESM
+//   target: "esnext",
+//   outDir: "dist",
+//   clean: true,
+//   bundle: true,
+//   splitting: false,
+//   sourcemap: true,
 
-  // Add this banner to shim require() for CJS dependencies
-  banner: {
-    js: `
-      import { createRequire } from 'module';
-      const require = createRequire(import.meta.url);
-    `,
-  },
-});
+//   // Add this banner to shim require() for CJS dependencies
+//   banner: {
+//     js: `
+//       import { createRequire } from 'module';
+//       const require = createRequire(import.meta.url);
+//     `,
+//   },
+// });
