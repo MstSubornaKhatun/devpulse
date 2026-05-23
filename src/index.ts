849c7b486478481cb37aa14f116f55c7d32aee3d
suborna33 <152749142+suborna33@users.noreply.github.com>
2026-05-23 17:34:09 +0600

    fix vercel serverless

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,22 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import authRoutes from './modules/auth/auth.routes';
 import issuesRoutes from './modules/issues/issues.routes';
- 
+
 dotenv.config();
+
 const app = express();
-const PORT = process.env.PORT || 5000;
- 
+
 app.use(cors());
 app.use(express.json());
+
 app.use('/api/auth', authRoutes);
 app.use('/api/issues', issuesRoutes);
- 
+
 app.get('/', (req, res) => {
-  res.json({ success: true, message: 'DevPulse API is running!' });
+  res.json({
+    success: true,
+    message: 'DevPulse API is running!',
+  });
 });
- 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 export default app;
